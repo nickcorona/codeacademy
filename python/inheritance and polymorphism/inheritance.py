948f9d8c2f3b671e93171ee1ab72fa779f14dbf1
Nick Corona <nicklucianocorona@gmail.com>
2019-04-06 20:19:13 -0400

    Started inheritance and polymorphism lesson.

--- python/inheritance and polymorphism/inheritance.py
+++ python/inheritance and polymorphism/inheritance.py
@@ -1,27 +1,24 @@
-class Bin:
+# Define your exception up here:
+class OutOfStock(Exception):
     pass
 
+# Update the class below to raise OutOfStock
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] == 0:
+        raise OutOfStock
+    else:
+        self.stock[color] = self.stock[color] - 1
 
-class RecyclingBin(Bin):
-    pass
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
 
-
-issubclass(ZeroDivisionError, Exception)
-
-
-class KitchenException(Exception):
-    """
-    Exception that gets thrown when a kitchen appliance isn't working
-    """
-
-
-class MicrowaveException(KitchenException):
-    """
-    Exception for when the microwave stops working
-    """
-
-
-class RefrigeratorException(KitchenException):
-    """
-    Exception for when the refrigerator stops working
-    """
+# This should raise OutOfStock:
+try:
+    candle_shop.buy('green')
+except OutOfStock:
+    print('Green is out of stock.')
---
+++ python/inheritance and polymorphism/interfaces.py
@@ -0,0 +1,3 @@
+class InsurancePolicy:
+    def __init__(self, price_of_item):
+        self.price_of_insured_item = price_of_item
