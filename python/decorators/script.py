f7628e5cd6801659d7b28afba39467ce410f96b0
Nick Corona <nicklucianocorona@gmail.com>
2019-04-10 20:37:51 -0400

    Finished python course.

---
+++ python/decorators/script.py
@@ -0,0 +1,13 @@
+def title_decorator(print_name_function):
+    def wrapper(*args, **kwargs):
+        print('Professor:')
+        print_name_function(*args, **kwargs)
+    return wrapper
+
+
+@title_decorator
+def print_my_name(name, age):
+    print(name + ' you are ' + str(age))
+
+
+print_my_name('Shelby', 12)
