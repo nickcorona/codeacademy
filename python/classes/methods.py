5b883b2d60c871b6a2705bb9a268c2699a26f645
Nick Corona <nicklucianocorona@gmail.com>
2019-04-13 23:09:53 -0400

    Fixed formatting.

--- python/classes/methods.py
+++ python/classes/methods.py
@@ -1,4 +1,4 @@
 class Rules:
-  def washing_brushes(self):
-    string = 'Point bristles towards the basin while washing your brushes.'
-    return string
+    def washing_brushes(self):
+        string = 'Point bristles towards the basin while washing your brushes.'
+        return string
